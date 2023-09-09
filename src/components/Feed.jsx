90313f69239ece8b70a8481ace655d6a8bdf048c
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2023-09-09 17:07:41 +0530

    search & users profile

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  // console.log(data)
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
@@ -21,8 +20,36 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  const handleSearchChange = (e) => {};
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchText, "i"); //"i" flag for case-insentitive
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+  
+  const tagClick = (tagName) => {
+    const searchedByTag = filterPrompts(tagName);
+    setSearchedResults(searchedByTag);
+    setSearchText(tagName);
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -45,8 +72,11 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardList data={searchedResults} handleTagClick={tagClick} />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={tagClick} />
+      )}
     </section>
   );
 };
